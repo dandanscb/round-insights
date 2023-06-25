81d7cfde93dd234dd687230eea1cb8acb247c980
Daniel Escobar <dandan.scb@hotmail.com>
2023-06-24 23:22:11 -0300

    add login and register

--- app/src/main/java/com/round/insights/app/login/view/RoundInsightsLoginActivity.kt
+++ app/src/main/java/com/round/insights/app/login/view/RoundInsightsLoginActivity.kt
@@ -2,13 +2,19 @@ package com.round.insights.app.login.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.round.insights.app.championship.view.ChampionshipActivity
 import com.round.insights.app.register.view.RoundInsightsRegisterActivity
 import com.round.insights.databinding.ActivityLoginBinding
 
 class RoundInsightsLoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +25,56 @@ class RoundInsightsLoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
+        binding.loginButton.setOnClickListener {
+            performLogin()
+        }
+
         binding.loginButtonRegister.setOnClickListener {
             val intent = Intent(this, RoundInsightsRegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun performLogin() {
+        val inputEmail = binding.loginEmail.text.toString()
+        val inputPassword = binding.loginPassword.text.toString()
+
+        if (inputEmail.isEmpty() || inputPassword.isEmpty()) {
+            Toast.makeText(
+                this,
+                "Please fill all fields",
+                Toast.LENGTH_SHORT,
+            ).show()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(inputEmail, inputPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, ChampionshipActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(
+                        baseContext,
+                        "Login Success.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    this,
+                    "Error occurred ${it.localizedMessage}",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
     }
 }
--- app/src/main/java/com/round/insights/app/register/view/RoundInsightsRegisterActivity.kt
+++ app/src/main/java/com/round/insights/app/register/view/RoundInsightsRegisterActivity.kt
@@ -2,13 +2,18 @@ package com.round.insights.app.register.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.round.insights.app.login.view.RoundInsightsLoginActivity
 import com.round.insights.databinding.ActivityRegisterBinding
 
 class RoundInsightsRegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +24,56 @@ class RoundInsightsRegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.registerButtonLogin.setOnClickListener {
-            val intent = Intent(this, RoundInsightsLoginActivity::class.java)
-            startActivity(intent)
+        auth = Firebase.auth
+
+        binding.registerButton.setOnClickListener {
+            performSignUp()
         }
+
+        binding.registerButtonLogin.setOnClickListener { startLoginActivity() }
+    }
+
+    private fun startLoginActivity() {
+        val intent = Intent(this, RoundInsightsLoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun performSignUp() {
+        val inputEmail = binding.registerEmail.text.toString()
+        val inputPassword = binding.registerPassword.text.toString()
+
+        if (inputEmail.isEmpty() || inputPassword.isEmpty()) {
+            Toast.makeText(
+                this,
+                "Please fill all fields",
+                Toast.LENGTH_SHORT,
+            ).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(inputEmail, inputPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startLoginActivity()
+                    Toast.makeText(
+                        this,
+                        "Register Success",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    this,
+                    "Error occurred ${it.localizedMessage}",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
     }
 }
